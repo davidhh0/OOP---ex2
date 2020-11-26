58e262df6575dde7b7a89bbdad1d675d946895fd
david <davidhh0h@gmail.com>
2020-11-26 16:42:35 +0200

    some changes

--- myBank.h
+++
@@ -1,8 +0,0 @@
-void O(double (*_accounts)[50][2] , int *_numOfAccounts);
-void B(double (*_accounts)[50][2]);
-void D(double (*_accounts)[50][2]);
-void W(double (*_accounts)[50][2]);
-void C(double (*_accounts)[50][2], int *_numOfAccounts);
-void I(double (*_accounts)[50][2]);
-void P(double (*_accounts)[50][2], int *_numOfAccounts);
-void FlushStdin();
