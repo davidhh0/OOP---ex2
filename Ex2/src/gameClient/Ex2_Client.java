736bc07baf59fc9ce4aa02642a4f154cdd31678e
יובל מרמר <yuvalmarmer97@gmail.com>
2020-12-05 14:10:22 +0200

    Delete all unrelevent comments

--- Ex2/src/gameClient/Ex2_Client.java
+++ Ex2/src/gameClient/Ex2_Client.java
@@ -30,7 +30,7 @@ public class Ex2_Client implements Runnable{
 	
 	@Override
 	public void run() {
-		int scenario_num = 23;
+		int scenario_num = 11;
 		game_service game = Game_Server_Ex2.getServer(scenario_num); // you have [0,23] games
 	 	//int id = 999;
 		//game.login(id);
@@ -71,18 +71,16 @@ public class Ex2_Client implements Runnable{
 		String lg = game.move();
 		List<CL_Agent> log = Arena.getAgents(lg, gg);
 		_ar.setAgents(log);
-		//ArrayList<OOP_Point3D> rs = new ArrayList<OOP_Point3D>();
 		String fs =  game.getPokemons();
 		List<CL_Pokemon> ffs = Arena.json2Pokemons(fs,gg);
 		_ar.setPokemons(ffs);
-//		pokemonEdges = FindPokemonsLocation(fs, gg);
 
 		//Priority queue from slowest agent to the fastest
 		PriorityQueue<CL_Agent> agentsQ = new PriorityQueue<>(new Comparator<CL_Agent>() {
 			@Override
 			public int compare(CL_Agent o1, CL_Agent o2) {
-				if(o1.getSpeed()>o2.getSpeed()) return 1;
-				if(o1.getSpeed()<o2.getSpeed()) return -1;
+				if(o1.getSpeed()>o2.getSpeed()) return -1;
+				if(o1.getSpeed()<o2.getSpeed()) return 1;
 				return 0;
 			}
 		});
@@ -104,79 +102,6 @@ public class Ex2_Client implements Runnable{
 				System.out.println("Agent: "+id+", val: "+v+"   turned to node: "+dest);
 			}
 		}
-
-//		for(int i=0;i<log.size();i++) {
-//			CL_Agent ag = log.get(i);
-//			int id = ag.getID();
-//			int dest = ag.getNextNode();
-//			int src = ag.getSrcNode();
-//			double v = ag.getValue();
-//			if(!OnceFlag){
-//				AgentMap agent = new AgentMap(id,0);
-//				AgentsMapping.put(id, agent);
-//			}
-//			if(dest==-1) {
-//				dest = nextNode(gg, src, id);
-//				game.chooseNextEdge(ag.getID(), dest);
-//				System.out.println("Agent: "+id+", val: "+v+"   turned to node: "+dest);
-//			}
-//		}
-//		OnceFlag=true;
-	}
-
-	private static ArrayList<PokemonEdge> FindPokemonsLocation(String pokemons, directed_weighted_graph graph){
-
-		Gson gson = new Gson();
-		Type JsonObjectType = new TypeToken<JsonObject>() {}.getType();
-		ArrayList<PokemonEdge> pokemon_Edges = new ArrayList<>();
-		JsonObject poke = gson.fromJson(pokemons, JsonObjectType);
-		JsonArray pok_array = poke.get("Pokemons").getAsJsonArray();
-
-		for(JsonElement pok : pok_array){
-
-			//For each pokemon need to find the edge he is on
-
-			JsonObject pokemon_obj = pok.getAsJsonObject();
-
-			JsonObject pokemon = pokemon_obj.get("Pokemon").getAsJsonObject();
-			String[] pok_location = pokemon.get("pos").getAsString().split(",");
-
-			double pokemon_x = Double.parseDouble(pok_location[0]);
-			double pokemon_y = Double.parseDouble(pok_location[1]);
-
-			geo_location pokemon_geo = new GeoLocation(pokemon_x,pokemon_y,0.0);
-
-			for(node_data node : graph.getV()){
-
-				for(edge_data edge : graph.getE(node.getKey())){
-					PokemonEdge pokemonEdge;
-					geo_location location1 = graph.getNode(edge.getSrc()).getLocation();
-					geo_location location2 = graph.getNode(edge.getDest()).getLocation();
-
-
-					//Type 1 = from big to small
-					//Type -1 = from small to big
-
-					if(Math.abs(location1.distance(pokemon_geo) + pokemon_geo.distance(location2) -  location1.distance(location2)) < Math.exp(-35)){
-						if(pokemon.get("type").getAsInt() == 1 && edge.getSrc() < edge.getDest()) {
-							//System.out.println(String.format("Found the pokemon on edge from %d to %d on coordinates (%f,%f) ", edge.getSrc(), edge.getDest(), (float) pokemon_geo.x(), (float) pokemon_geo.y()));
-
-							int id = (int)(pokemon.get("type").getAsInt() * pokemon.get("value").getAsInt() * (pokemon_x - pokemon_y)*1000000);
-							pokemonEdge = new PokemonEdge(edge.getSrc(),edge.getDest(),pokemon.get("value").getAsDouble(),id);
-							pokemon_Edges.add(pokemonEdge);
-						}
-						else if(pokemon.get("type").getAsInt() == -1 && edge.getSrc() > edge.getDest()){
-							//System.out.println(String.format("Found the pokemon on edge from %d to %d on coordinates (%f,%f) ", edge.getSrc(), edge.getDest(), (float) pokemon_geo.x(), (float) pokemon_geo.y()));
-							int id = (int)(pokemon.get("type").getAsInt() * pokemon.get("value").getAsInt() * (pokemon_x - pokemon_y)*1000000);
-							pokemonEdge = new PokemonEdge(edge.getSrc(),edge.getDest(),pokemon.get("value").getAsDouble(),id);
-							pokemon_Edges.add(pokemonEdge);
-						}
-					}
-				}
-			}
-		}
-
-		return pokemon_Edges;
 
 	}
 
